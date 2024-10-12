21aab0040024c89aba6c05ad9015bf64d2c8536c
swimdoraemon <indoaradhya@gmail.com>
2024-10-12 11:21:11 +0900

    nested loops

---
+++ character occurrence.py
@@ -0,0 +1,12 @@
+string = input("Please enter your own word : ")
+char = input("Please enter your own character : ")
+i = 0
+count = 0
+
+while(i < len(string)):
+
+    if(string[i] == char):
+        count = count + 1
+    i = i + 1
+
+print("The total Number of times ", char," has Occurred = ", count)
